aaa57f19f9270bd9ad96e752f98335635e7e776b
Erudite885 <erudite885@gmail.com>
2022-06-14 11:13:56 +0100

    submitHandler set, 2-way binding achieved

--- src/components/CourseGoals/CourseInput.js
+++ src/components/CourseGoals/CourseInput.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 import styles from './CourseInput.module.css';
 
 const CourseInput = () => {
 
+    const [enteredTitle, setEnteredTitle] = useState('');
+
+    const titleChangeHandler = (e) => {
+        setEnteredTitle(e.target.value);
+        // console.log(e.target.value)
+    };
+
     const submitHandler = (e) => {
         e.preventDefault();
-    }
+        // console.log(e)
+        // enteredTitle('')
+        const data = {
+            title: enteredTitle
+        }
+        console.log(data)
+        setEnteredTitle('')
+    };
 
 
   return (
@@ -14,7 +29,7 @@ const CourseInput = () => {
         <form onSubmit={submitHandler}>
             <div className={styles.formControl}>
                 <label className={styles.label}>Course Goal </label>
-                <input type="text" className={styles.input} />
+                <input type="text" value={enteredTitle} onChange={titleChangeHandler} className={styles.input} />
             </div>
             <button type="submit" className={styles.button}> Add Goal </button>
         </form>
