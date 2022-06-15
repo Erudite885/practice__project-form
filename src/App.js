689fad99caaa46108406b3661e0c1b147cc02d3e
Erudite885 <erudite885@gmail.com>
2022-06-16 00:45:04 +0100

    CourseGoals state added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+// import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
 import CourseInput from './components/CourseGoals/CourseInput';
 import './App.css';
 
+
 const App = () => {
+    const [CourseGoals, setCourseGoals] = useState([
+        {
+            id: 'g1',
+            text: 'Course Overview'
+        }, 
+        {
+            id: 'g2',
+            text: 'Course Preview'
+        }, 
+        {
+            id: 'g3',
+            text: 'Course Outline'
+        }
+    ])
+
   return (
   <div>
     <div>CourseGoals App</div>
     <CourseInput />
+    {/* <section id='goals'>
+        {content}
+    </section> */}
   </div>
   );
 }
